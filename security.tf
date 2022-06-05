aee730b063963540636937f63bb355e71224241d
vasamsetti-22 <vasamsettiprathyusha@gmail.com>
2022-06-05 18:37:07 +0530

    adding security groups

---
+++ security.tf
@@ -0,0 +1,49 @@
+
+resource "aws_security_group" "lb-sg" {
+  name        = "load-balancer-sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks =  ["10.0.0.0/17"]
+    
+  }
+  tags = {
+    "Name" = "lb-sg"
+  }
+
+}
+resource "aws_security_group" "instance-sec" {
+  name        = "instance-sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups = [aws_security_group.lb-sg.id]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
+  }
+  tags = {
+    "Name" = "instance-sg"
+  }
+
+}
